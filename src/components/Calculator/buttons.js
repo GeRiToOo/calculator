bd9f6d13438ccb0ef23a224b20974866b3e8bc34
GeRiToOo <57684252+GeRiToOo@users.noreply.github.com>
2020-06-11 00:32:48 +0100

    last update

---
+++ src/components/Calculator/buttons.js
@@ -0,0 +1,97 @@
+export const buttons = [
+  {
+    label: "c",
+    type: "clear",
+    class: "clear",
+  },
+  {
+    label: "+/-",
+    type: "negativePositive",
+    class: "negativePositive",
+  },
+  {
+    label: "%",
+    type: "percentage",
+    class: "percentage",
+  },
+  {
+    label: "/",
+    type: "symbol",
+    class: "symbol",
+  },
+  {
+    label: 7,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: 8,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: 9,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: "*",
+    type: "symbol",
+    class: "symbol",
+  },
+  {
+    label: 4,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: 5,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: 6,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: "-",
+    type: "symbol",
+    class: "symbol",
+  },
+  {
+    label: 1,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: 2,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: 3,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: "+",
+    type: "symbol",
+    class: "symbol",
+  },
+  {
+    label: 0,
+    type: "number",
+    class: "number",
+  },
+  {
+    label: ".",
+    type: "decimal",
+    class: "decimal",
+  },
+  {
+    label: "=",
+    type: "symbol",
+    class: "equal",
+  },
+];
